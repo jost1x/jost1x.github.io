34a7e0cbe55d5743456a66e0cd2e9263d17e86b4
Charlie Melchiori <charlie.melchiorip@gmail.com>
2023-12-31 20:25:04 -0300

    add purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  output: "static",
+  integrations: [tailwind(), icon(), purgecss()],
+  output: "static"
 });
